d66c997c2f50c10b3e50b9d0a53ed00b8bfab253
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2024-02-24 23:39:42 +0300

    Added scripts to populate tables

---
+++ scripts/import-seat-data.ts
@@ -0,0 +1,117 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+
+// Initialize DynamoDB client
+const dynamodb = new DynamoDB({});
+
+const table_name = "SeatBooking";
+
+// Sample data
+let data = [
+  { FlightID: "FL123", SeatID: "1A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "1B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "1C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "1D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "1E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "1F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "2F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "3F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "4F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "5F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "6F", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7A", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7B", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7C", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7D", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7E", IsBooked: "false" },
+  { FlightID: "FL123", SeatID: "7F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "1F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "2F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "3F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "4F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "5F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "6F", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7A", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7B", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7C", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7D", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7E", IsBooked: "false" },
+  { FlightID: "FL456", SeatID: "7F", IsBooked: "false" },
+];
+
+// Insert data into DynamoDB table
+const putItems = async () => {
+  while (data.length) {
+    const batchCommand = {
+      RequestItems: {
+        SeatBooking: data.splice(0, 20).map((item) => ({
+          PutRequest: {
+            Item: marshall(item),
+          },
+        })),
+      },
+    };
+
+    const res = await dynamodb.batchWriteItem(batchCommand);
+    console.log(res);
+  }
+};
+
+putItems();
