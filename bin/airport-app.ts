c8a8ed0f590f817d10df81ec5d8d78057c3d1c3f
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2024-02-24 23:41:54 +0300

    Added frontent

--- bin/airport-app.ts
+++ bin/airport-app.ts
@@ -4,10 +4,18 @@ import * as cdk from "aws-cdk-lib";
 import { AirportAppStack } from "../lib/airport-app-stack";
 import { DatabaseStack } from "../lib/database-stack";
 import { AddUserLambdaStack } from "../lib/lambda-confirm.stack";
+import { AuthStack } from "../lib/auth-stack";
 
 const app = new cdk.App();
-const databaseStack = new DatabaseStack(app, "DatabaseStack");
+
+const databaseStack = new DatabaseStack(app, "DatabaseStack", {
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
 
 const addUserLambdaFunc = new AddUserLambdaStack(app, "AddUserLambdaStack", {
   table: databaseStack.usersTable,
+});
+
+const authStack = new AuthStack(app, "AuthStack", {
+  addUserPostConfirmation: addUserLambdaFunc.addUserToTableFunc,
 });
